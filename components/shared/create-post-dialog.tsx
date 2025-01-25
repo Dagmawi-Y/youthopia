eb81719f01d696f9651290435aa2906997ddc721
Dagmawi-Y <dagmawi.inbox@gmail.com>
2025-01-26 00:29:25 +0300

    Customize emoji picker in CreatePostDialog

--- components/shared/create-post-dialog.tsx
+++ components/shared/create-post-dialog.tsx
@@ -127,7 +127,50 @@ export function CreatePostDialog() {
             </button>
             {showEmojiPicker && (
               <div className="absolute right-0 top-10 z-50">
-                <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+                <Picker
+                  data={data}
+                  onEmojiSelect={handleEmojiSelect}
+                  categories={[
+                    "frequent",
+                    "people",
+                    "nature",
+                    "foods",
+                    "activity",
+                    "places",
+                    "objects",
+                    "symbols",
+                  ]}
+                  exceptEmojis={[
+                    "middle_finger",
+                    "fu",
+                    "hankey",
+                    "poop",
+                    "shit",
+                    "skull",
+                    "skull_and_crossbones",
+                    "smoking",
+                    "cigarette",
+                    "knife",
+                    "gun",
+                    "bomb",
+                    "man-kiss-man",
+                    "woman-kiss-woman",
+                    "man-heart-man",
+                    "woman-heart-woman",
+                    "man-man-boy",
+                    "man-man-boy-boy",
+                    "man-man-girl",
+                    "man-man-girl-boy",
+                    "man-man-girl-girl",
+                    "woman-woman-boy",
+                    "woman-woman-boy-boy",
+                    "woman-woman-girl",
+                    "woman-woman-girl-boy",
+                    "woman-woman-girl-girl",
+                    "couplekiss",
+                    "family",
+                  ]}
+                />
               </div>
             )}
           </div>
